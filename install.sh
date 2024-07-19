56c7aa20dc8b13f2e0cb12bc9945637476ef4ade
trifoil <unixsslayer@gmail.com>
2024-07-20 01:56:47 +0200

    update on main menu

--- install.sh
+++ install.sh
@@ -19,14 +19,31 @@ display_menu() {
     echo "|              Please select the tool you want to use                  |"
     echo "|----------------------------------------------------------------------|"
     echo "| 0. Say Hello                                                         |"
+    echo "| 1. SSH connection                                                    |"
+    echo "| 2. NFS/SAMBA shared directory (no auth)                              |"
+    echo "| 3. Users Management Menu                                             |"
+    #contains
+    #server, mail, users management, domain mangement
+    #
+    echo "| 4. NTP Time Server                                                   |"
+    echo "| 5. Security Settings                                                 |"
+    echo "| 6. Consult Logs Dashboard                                            |"
     echo "|----------------------------------------------------------------------|"
     echo "| q. Quit                                                              |"
     echo "|----------------------------------------------------------------------|"
     echo ""
 }
 
-sayhello(){
+0(){
     echo "hello"
+}
+
+1(){
+    echo test
+}
+
+2(){
+
 }
 
 # Main function
@@ -36,6 +53,8 @@ main() {
         read -p "Enter your choice: " choice
         case $choice in
             0) sayhello ;;
+            1) 1 ;;
+            2) 2 ;;
             q|Q) clear && echo "Exiting the web server configuration wizard." && exit ;;
             *) clear && echo "Invalid choice. Please enter a valid option." ;;
         esac
