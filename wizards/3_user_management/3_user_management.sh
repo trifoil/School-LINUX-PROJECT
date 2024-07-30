5dd3a0d6453d1d44ed0214cc75e2f08790145e41
trifoil <unixsslayer@gmail.com>
2024-07-30 18:44:58 +0200

    ip nice and ready

--- wizards/3_user_management/3_user_management.sh
+++ wizards/3_user_management/3_user_management.sh
@@ -27,6 +27,10 @@ ip_set(){
     nmcli connection modify "$INTERFACE" ipv4.method manual
     nmcli connection modify "$INTERFACE" ipv4.addresses "$ADDRESS/24"
     nmcli connection up "$INTERFACE"
+    echo "Done..."
+    echo "Press any key to continue..."
+    read -n 1 -s key
+	clear
 }
 
 backup_file(){
