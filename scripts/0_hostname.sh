7b08e79d44a49d544308d3d6dca682c4553dce07
trifoil <unixsslayer@gmail.com>
2024-08-04 20:13:59 +0200

    hostname done

--- scripts/0_hostname.sh
+++ scripts/0_hostname.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-PINK='\x1b[38;5;162m' 
+BLUE='\e[38;5;33m'
 CURRENT_HOSTNAME=$(hostname)
 
-clear
 display_menu() {
     echo ""
     echo "|----------------------------------------------------------------------|"
-    echo -e "|                   ${PINK}Hostname assistant${PINK}                   |"
+    echo -e "|                ${BLUE}Welcome to the hostname assistant ${NC}                    |"
+    echo "|                  Please input your new hostname                      |"
     echo "|----------------------------------------------------------------------|"
+    echo ""
+    echo -e "Current : ${CURRENT_HOSTNAME}"
+    echo ""
 }
 
 display_menu
 read -p "Enter your choice (q to abort) : " choice
 case $HOSTNAME in
             q|Q) clear && echo "Exiting ... " && exit ;;
-            *) hostname $HOSTNAME && echo "Invalid choice. Please enter a valid option." ;;
+            *) hostname $HOSTNAME && echo "Done" && exit;;
 esac
