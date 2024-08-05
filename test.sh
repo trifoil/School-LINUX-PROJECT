9ec21dd0878b32e0ad81338cf4b97ed2750d21ef
trifoil <unixsslayer@gmail.com>
2024-08-05 22:07:24 +0200

    a

--- test.sh
+++ test.sh
@@ -58,7 +58,7 @@ options {
     dump-file "/var/named/data/cache_dump.db";
     statistics-file "/var/named/data/named_stats.txt";
     memstatistics-file "/var/named/data/named_mem_stats.txt";
-    allow-query { localhost; internals; };
+    allow-query { any; };
     recursion yes;
 };
 
