8f27388f217cc936318e9adf158c4d24a6114a84
trifoil <unixsslayer@gmail.com>
2024-08-05 22:01:10 +0200

    a

--- test.sh
+++ test.sh
@@ -54,42 +54,13 @@ basic_dns(){
 
 cat <<EOL > /etc/named.conf
 options {
-        // change ( listen all )
-        listen-on port 53 { any; };
-        // change if need ( if not listen IPv6, set [none] )
-        listen-on-v6 { any; };
-        directory       "/var/named";
-        dump-file       "/var/named/data/cache_dump.db";
-        statistics-file "/var/named/data/named_stats.txt";
-        memstatistics-file "/var/named/data/named_mem_stats.txt";
-        secroots-file   "/var/named/data/named.secroots";
-        recursing-file  "/var/named/data/named.recursing";
-        // add local network set on [acl] section above
-        // network range you allow to receive queries from hosts
-        allow-query     { localhost; internal-network; };
-        // network range you allow to transfer zone files to clients
-        // add secondary DNS servers if it exist
-        allow-transfer  { localhost; };
-
-
-        recursion yes;
-
-        dnssec-validation yes;
-
-        managed-keys-directory "/var/named/dynamic";
-
-        pid-file "/run/named/named.pid";
-        session-keyfile "/run/named/session.key";
-
-        /* https://fedoraproject.org/wiki/Changes/CryptoPolicy */
-        include "/etc/crypto-policies/back-ends/bind.config";
+    directory "/var/named";
+    dump-file "/var/named/data/cache_dump.db";
+    statistics-file "/var/named/data/named_stats.txt";
+    memstatistics-file "/var/named/data/named_mem_stats.txt";
+    allow-query { localhost; internal-network; };
+    recursion yes;
 };
-
-logging {
-        channel default_debug {
-                file "data/named.run";
-                severity dynamic;
-        };
 
 zone "$DOMAIN_NAME" IN {
     type master;
