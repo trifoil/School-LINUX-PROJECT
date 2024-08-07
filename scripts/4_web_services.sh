04b3271ad6e063bc6b1576c3a384f78e11e2e384
trifoil <unixsslayer@gmail.com>
2024-08-07 12:19:57 +0200

    a

--- scripts/4_web_services.sh
+++ scripts/4_web_services.sh
@@ -68,12 +68,27 @@ basic_dns(){
 
 cat <<EOL > /etc/named.conf
 options {
+    listen-on port 53 { any; };
     directory "/var/named";
     dump-file "/var/named/data/cache_dump.db";
     statistics-file "/var/named/data/named_stats.txt";
     memstatistics-file "/var/named/data/named_mem_stats.txt";
+    secroots-file   "/var/named/data/named.secroots";
+    recursing-file  "/var/named/data/named.recursing";
     allow-query { any; };
     recursion yes;
+};
+
+logging {
+        channel default_debug {
+                file "data/named.run";
+                severity dynamic;
+        };
+};
+
+zone "." IN {
+        type hint;
+        file "named.ca";
 };
 
 zone "$DOMAIN_NAME" IN {
